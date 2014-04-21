842a428f55c14be64db105c3f2da01a8290cb776
Geoff <me@geoffreychong.com>
2014-04-21 12:16:03 +1000

    Added Seeders

--- app/database/seeds/CommentsTableSeeder.php
+++ app/database/seeds/CommentsTableSeeder.php
@@ -1,20 +1,35 @@
 <?php
-
-// Composer: "fzaninotto/faker": "v1.3.0"
-use Faker\Factory as Faker;
-
+ 
 class CommentsTableSeeder extends Seeder {
-
-	public function run()
-	{
-		$faker = Faker::create();
-
-		foreach(range(1, 10) as $index)
-		{
-			Comment::create([
-
-			]);
-		}
-	}
-
+ 
+  public function run()
+  {
+    $comments = array(
+      array(
+        'content'   => 'Lorem ipsum Nisi dolore ut incididunt mollit tempor proident eu velit cillum dolore sed',
+        'author_name' => 'Testy McTesterson',
+        'post_id'   => 1,
+        'created_at' => date('Y-m-d H:i:s'),
+        'updated_at' => date('Y-m-d H:i:s'),
+      ),
+      array(
+        'content'   => 'Lorem ipsum Nisi dolore ut incididunt mollit tempor proident eu velit cillum dolore sed',
+        'author_name' => 'Testy McTesterson',
+        'post_id'   => 1,
+        'created_at' => date('Y-m-d H:i:s'),
+        'updated_at' => date('Y-m-d H:i:s'),
+      ),
+      array(
+        'content'   => 'Lorem ipsum Nisi dolore ut incididunt mollit tempor proident eu velit cillum dolore sed',
+        'author_name' => 'Testy McTesterson',
+        'post_id'   => 2,
+        'created_at' => date('Y-m-d H:i:s'),
+        'updated_at' => date('Y-m-d H:i:s'),
+      ),
+    );
+ 
+    // Uncomment the below to run the seeder
+    DB::table('comments')->insert($comments);
+  }
+ 
 }
--- app/database/seeds/PostsTableSeeder.php
+++ app/database/seeds/PostsTableSeeder.php
@@ -1,20 +1,28 @@
 <?php
 
-// Composer: "fzaninotto/faker": "v1.3.0"
-use Faker\Factory as Faker;
-
 class PostsTableSeeder extends Seeder {
-
-	public function run()
-	{
-		$faker = Faker::create();
-
-		foreach(range(1, 10) as $index)
-		{
-			Post::create([
-
-			]);
-		}
-	}
-
+ 
+  public function run()
+  {
+    $posts = array(
+      array(
+        'title'    => 'Test Post',
+        'content'   => 'Lorem ipsum Reprehenderit velit est irure in enim in magna aute occaecat qui velit ad.',
+        'author_name' => 'Conar Welsh',
+        'created_at' => date('Y-m-d H:i:s'),
+        'updated_at' => date('Y-m-d H:i:s'),
+      ),
+      array(
+        'title'    => 'Another Test Post',
+        'content'   => 'Lorem ipsum Reprehenderit velit est irure in enim in magna aute occaecat qui velit ad.',
+        'author_name' => 'Conar Welsh',
+        'created_at' => date('Y-m-d H:i:s'),
+        'updated_at' => date('Y-m-d H:i:s'),
+      ),
+    );
+ 
+    // Uncomment the below to run the seeder
+    DB::table('posts')->insert($posts);
+  }
+ 
 }
