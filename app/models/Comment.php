9f4edb9e4068942454e49b7a6ebf95423b6b1fb2
Geoff <me@geoffreychong.com>
2014-04-21 12:39:40 +1000

    Added Post and Comment models

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -1,5 +1,38 @@
 <?php
-
-class Comment extends \Eloquent {
-	protected $fillable = [];
+/**
+ * Represent a Comment Item, or Collection
+ */
+class Comment extends Eloquent {
+ 
+  /**
+   * Items that are "fillable"
+   * meaning we can mass-assign them from the constructor
+   * or $comment->fill()
+   * @var array
+   */
+  protected $fillable = array(
+    'post_id', 'content', 'author_name'
+  );
+ 
+  /**
+   * Validation Rules
+   * this is just a place for us to store these, you could
+   * alternatively place them in your repository
+   * @var array
+   */
+  public static $rules = array(
+    'post_id'   => 'required|numeric',
+    'content'   => 'required',
+    'author_name' => 'required'
+  );
+ 
+  /**
+   * Define the relationship with the posts table
+   * @return Model parent Post model
+   */
+  public function post()
+  {
+    return $this->belongsTo('Post');
+  }
+ 
 }
--- app/models/Post.php
+++ app/models/Post.php
@@ -1,5 +1,37 @@
 <?php
-
-class Post extends \Eloquent {
-	protected $fillable = [];
+/**
+ * Represent a Post Item, or Collection
+ */
+class Post extends Eloquent {
+ 
+  /**
+   * Items that are "fillable"
+   * meaning we can mass-assign them from the constructor
+   * or $post->fill()
+   * @var array
+   */
+  protected $fillable = array(
+    'title', 'content', 'author_name'
+  );
+ 
+  /**
+   * Validation Rules
+   * this is just a place for us to store these, you could
+   * alternatively place them in your repository
+   * @var array
+   */
+  public static $rules = array(
+    'title'    => 'required',
+    'author_name' => 'required'
+  );
+ 
+  /**
+   * Define the relationship with the comments table
+   * @return Collection collection of Comment Models
+   */
+  public function comments()
+  {
+    return $this->hasMany('Comment');
+  }
+ 
 }
